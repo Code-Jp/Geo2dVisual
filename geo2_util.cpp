852d5ce220dc4cbeae72d18c34a0d8b0910ac956
Jinpan Zheng <epjpzheng@gmail.com>
2024-02-22 14:41:13 -0500

    Complete class implementation for point, segment, circle and triangle; triangle remains untested

--- geo2_util.cpp
+++ geo2_util.cpp
@@ -6,18 +6,14 @@
 #include "geo2_util.h"
 
 namespace Geo2Util {
-    std::string Black() {
-        static std::string black(" 0 0 0 255");
-        return black;
-    }
 
     /**
-     * @brief Convert BoundaryType string with its x and y coordinate
+     * @brief Convert BoundaryType to string
      * @param t Boundary Type
      * @return A string object containing the representation of BoundaryType
      */
-    std::string toString(const BoundaryType& t) {
-        switch (t) {
+    std::string toString(const BoundaryType& bt) {
+        switch (bt) {
             case BoundaryType::Solid : return "0";
             case BoundaryType::Dotted : return "1";
             case BoundaryType::Dashed : return "2";
@@ -26,6 +22,31 @@ namespace Geo2Util {
     }
 
     /**
+     * @brief Convert color to string in a form of "r, g, b, trans"
+     * @param color Color object (r, b, g, trans)
+     * @return A string object containing the representation of Color 
+     */
+    std::string toString(const Color& color)
+    {
+    return std::to_string(color.r) + " "
+                + std::to_string(color.g) + " "
+                + std::to_string(color.b) + " "
+                + std::to_string(color.trans);
+    }
+
+    void printToFile(const std::string& filename, const std::vector<std::string>& geo2_Objects) {
+
+        std::ofstream output(filename);
+        output << std::fixed << std::setprecision(10);
+        for (int i = 0; i < geo2_Objects.size(); i++) {
+            output << geo2_Objects[i] << std::endl;
+        }
+        output.close();
+    }
+
+// Default toString: toString(CGAL::Kernel::Object)
+// Default color = Color::OpaqueBlack = Color{0, 0, 0, 255}; default boundary type = BoundaryType::Solid
+    /**
      * @brief Convert Point_2 object to string with its x and y coordinate
      * @param p Point_2 object
      * @return A string object containing the representation of Point_2 object
@@ -33,8 +54,11 @@ namespace Geo2Util {
     std::string toString(const Point_2& p) {
 
         std::ostringstream s;
-        s << std::fixed << std::setprecision(10) << "POINT " << p.x() << " " << p.y() 
-            << Black() + toString(BoundaryType::Solid) + Black();
+        s << std::fixed << std::setprecision(10) << "POINT " << p.x() << " " 
+            << p.y() << " "
+            << toString(Geo2Util::OpaqueBlack) << " "
+            << toString(BoundaryType::Solid) << " " 
+            << toString(Geo2Util::OpaqueBlack);
 
         return s.str();
     }
@@ -46,7 +70,8 @@ namespace Geo2Util {
      */
     std::string toString(const Segment_2& seg) {
 
-        return "LINE_SEGMENT" + Black() + toString(BoundaryType::Dotted) + "\n"
+        return "LINE_SEGMENT " + toString(Geo2Util::OpaqueBlack) + " " 
+                + toString(BoundaryType::Solid) + "\n"
                 + toString(seg.source()) + "\n"
                 + toString(seg.target());
     }
@@ -57,12 +82,13 @@ namespace Geo2Util {
      * @return A string object containing the representation of Circle_2 object
      */
     std::string toString(const Circle_2& circ) {
-
         double raduis = std::sqrt(circ.squared_radius());
 
         std::ostringstream c;
-        c << std::fixed << std::setprecision(10) << "CIRCLE " << raduis 
-            << Black() << toString(BoundaryType::Dashed) << Black() + "\n"
+        c << std::fixed << std::setprecision(10) << "CIRCLE " << raduis << " " 
+            << toString(Geo2Util::OpaqueBlack) << " "
+            << toString(BoundaryType::Solid) << " "
+            << toString(Geo2Util::OpaqueBlack) + "\n"
             << toString(circ.center());
 
         return c.str();
@@ -75,7 +101,9 @@ namespace Geo2Util {
      */
     std::string toString(const Triangle_2& tri) {
 
-        return "TRIANGLE" + Black() + toString(BoundaryType::Dotted) + Black() + "\n"
+        return "TRIANGLE " + toString(Geo2Util::OpaqueBlack) + " " 
+                + toString(BoundaryType::Solid) + " " 
+                + toString(Geo2Util::OpaqueBlack) + "\n"
                 + toString(tri[0]) + "\n"
                 + toString(tri[1]) + "\n"
                 + toString(tri[2]);
@@ -88,18 +116,256 @@ namespace Geo2Util {
      */
     std::string toString(const Iso_rectangle_2& rect) {
 
-        return "RECTANGLE" + Black() + toString(BoundaryType::Dotted) + Black() + "\n"
+        return "RECTANGLE " + toString(Geo2Util::OpaqueBlack) + " "
+                + toString(BoundaryType::Solid) + " "
+                + toString(Geo2Util::OpaqueBlack) + "\n"
                 + toString(rect.min()) + "\n"
                 + toString(rect.max());
     }
 
-    void printToFile(const std::string& filename, const std::vector<std::string>& geo2_Objects) {
+// Customized toString: toString(KernelObject_Visual)
+    std::string toString(const Point_2_Visual& pv) {
+        std::ostringstream s;
+        s << std::fixed << std::setprecision(10) << "POINT " << pv.x() << " " 
+            << pv.y() << " "
+            << toString(pv.getBondaryColor()) << " " 
+            << toString(pv.getBoundaryType()) << " "
+            << toString(pv.getInteriorColor());
 
-        std::ofstream output(filename);
-        output << std::fixed << std::setprecision(10);
-        for (int i = 0; i < geo2_Objects.size(); i++) {
-            output << geo2_Objects[i] << std::endl;
-        }
-        output.close();
+        return s.str();
     }
-}
+
+    std::string toString(const Segment_2_Visual& segv) {
+
+        return "LINE_SEGMENT " + toString(segv.getBondaryColor()) + " " 
+                + toString(segv.getBoundaryType()) + "\n"
+                + toString(segv.source()) + "\n"
+                + toString(segv.target());
+    }
+
+    std::string toString(const Circle_2_Visual& circv) {
+        double raduis = std::sqrt(circv.squared_radius());
+
+        std::ostringstream c;
+        c << std::fixed << std::setprecision(10) << "CIRCLE " << raduis << " " 
+            << toString(circv.getBondaryColor()) << " "
+            << toString(circv.getBoundaryType()) << " "
+            << toString(circv.getInteriorColor()) + "\n"
+            << toString(circv.center());
+
+        return c.str();
+    }
+
+    std::string toString(const Triangle_2_Visual& triv) {
+
+        return "TRIANGLE " + toString(triv.getBondaryColor()) + " " 
+                + toString(triv.getBoundaryType()) + " " 
+                + toString(triv.getInteriorColor()) + "\n"
+                + toString(triv[0]) + "\n"
+                + toString(triv[1]) + "\n"
+                + toString(triv[2]);
+    }
+    
+// Visual Wrapper Classes:
+// Point_2_Visual
+    Point_2_Visual::Point_2_Visual(const Point_2& p)
+        : m_p(p.x(), p.y())
+            , m_boundaryColor{OpaqueBlack}
+            , m_interiorColor{OpaqueBlack}
+            , m_bType {BoundaryType::Solid} {
+    }
+
+    Point_2_Visual::Point_2_Visual(const Point_2& p, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype) 
+        : m_p(p.x(), p.y())
+            , m_boundaryColor{boudaryColor}
+            , m_interiorColor{interiorColor}
+            , m_bType {btype} {
+    }
+
+    void Point_2_Visual::setBondaryColor(const Color& color) {
+        m_boundaryColor = color;
+    }
+
+    Color Point_2_Visual::getBondaryColor() const {
+        return m_boundaryColor;
+    }
+
+    void Point_2_Visual::setInteriorColor(const Color& color) {
+        m_interiorColor = color;
+    }
+
+    Color Point_2_Visual::getInteriorColor() const {
+        return m_interiorColor;
+    }
+
+    void Point_2_Visual::setBoundaryType(const BoundaryType& btype) {
+        m_bType = btype;
+    }
+
+    BoundaryType Point_2_Visual::getBoundaryType() const {
+        return m_bType;
+    }
+
+    Point_2 Point_2_Visual::KernelObject() const {
+        return Point_2(m_p.x(), m_p.y());
+    }
+
+    K::FT Point_2_Visual::x() const {
+        return m_p.x();
+    }
+
+    K::FT Point_2_Visual::y() const {
+        return m_p.y();
+    }
+
+// Segment_2_Visual
+    Segment_2_Visual::Segment_2_Visual(const Point_2_Visual& s, const Point_2_Visual& t)
+        : m_source(s)
+            , m_target(t)
+            , m_boundaryColor{OpaqueBlack}
+            , m_bType {BoundaryType::Solid} {
+    }
+
+    Segment_2_Visual::Segment_2_Visual(const Point_2_Visual& s, const Point_2_Visual& t, const Color& boudaryColor, const BoundaryType& btype)
+        : m_source(s)
+            , m_target(t)
+            , m_boundaryColor{boudaryColor}
+            , m_bType{btype} {
+    }
+
+    void Segment_2_Visual::setBondaryColor(const Color& color) {
+        m_boundaryColor = color;
+    }
+
+    Color Segment_2_Visual::getBondaryColor() const {
+        return m_boundaryColor;
+    }
+
+    void Segment_2_Visual::setBoundaryType(const BoundaryType& btype) {
+        m_bType = btype;
+    }
+
+    BoundaryType Segment_2_Visual::getBoundaryType() const {
+        return m_bType;
+    }
+
+    Segment_2 Segment_2_Visual::KernelObject() const {
+        return Segment_2(m_source.KernelObject(), m_target.KernelObject());
+    }
+
+    Point_2_Visual Segment_2_Visual::source() const {
+        return m_source;
+    }
+    Point_2_Visual Segment_2_Visual::target() const {
+        return m_target;
+    }
+
+// Circle_2_Visual
+    Circle_2_Visual::Circle_2_Visual(const Point_2_Visual& center, K::FT squared_radius)
+        : m_center(center)
+            , m_squaredRadius{squared_radius}
+            , m_boundaryColor{OpaqueBlack}
+            , m_interiorColor{OpaqueBlack}
+            , m_bType{BoundaryType::Solid} {
+    }
+
+    Circle_2_Visual::Circle_2_Visual(const Point_2_Visual& center, K::FT squared_radius, const Color& boundaryColor, const Color& interiorColor, const BoundaryType& btype)
+        : m_center(center)
+            , m_squaredRadius{squared_radius}
+            , m_boundaryColor{boundaryColor}
+            , m_interiorColor{interiorColor}
+            , m_bType{btype} {
+    }
+
+    void Circle_2_Visual::setBondaryColor(const Color& color) {
+        m_boundaryColor = color;
+    }
+
+    Color Circle_2_Visual::getBondaryColor() const {
+        return m_boundaryColor;
+    }
+
+    void Circle_2_Visual::setInteriorColor(const Color& color) {
+        m_interiorColor = color;
+    }
+
+    Color Circle_2_Visual::getInteriorColor() const {
+        return m_interiorColor;
+    }
+
+    void Circle_2_Visual::setBoundaryType(const BoundaryType& btype) {
+        m_bType = btype;
+    }
+
+    BoundaryType Circle_2_Visual::getBoundaryType() const {
+        return m_bType;
+    }
+
+    Circle_2 Circle_2_Visual::KernelObject() const {
+        return Circle_2(m_center.KernelObject(), m_squaredRadius);
+    }
+
+    Point_2_Visual Circle_2_Visual::center() const {
+        return m_center;
+    }
+
+    K::FT Circle_2_Visual::squared_radius() const {
+        return m_squaredRadius;
+    }
+
+// Triangle_2_Visual
+    Triangle_2_Visual::Triangle_2_Visual(const Point_2_Visual& p, const Point_2_Visual& q, const Point_2_Visual& r) 
+        : m_boundaryColor{OpaqueBlack}
+            , m_interiorColor{OpaqueBlack}
+            , m_bType{BoundaryType::Solid} {
+        m_vertices.reserve(3);
+        m_vertices.emplace_back(Point_2_Visual(p));
+        m_vertices.emplace_back(Point_2_Visual(q));
+        m_vertices.emplace_back(Point_2_Visual(r));
+    }
+
+    Triangle_2_Visual::Triangle_2_Visual(const Point_2_Visual& p, const Point_2_Visual& q, const Point_2_Visual& r, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype) 
+        : m_boundaryColor{boudaryColor}
+            , m_interiorColor{interiorColor}
+            , m_bType{btype} {
+        m_vertices.reserve(3);
+        m_vertices.emplace_back(Point_2_Visual(p));
+        m_vertices.emplace_back(Point_2_Visual(q));
+        m_vertices.emplace_back(Point_2_Visual(r));
+    }
+
+    void Triangle_2_Visual::setBondaryColor(const Color& color) {
+        m_boundaryColor = color;
+    }
+
+    Color Triangle_2_Visual::getBondaryColor() const {
+        return m_boundaryColor;
+    }
+
+    void Triangle_2_Visual::setInteriorColor(const Color& color) {
+        m_interiorColor = color;
+    }
+
+    Color Triangle_2_Visual::getInteriorColor() const {
+        return m_interiorColor;
+    }
+
+    void Triangle_2_Visual::setBoundaryType(const BoundaryType& btype) {
+        m_bType = btype;
+    }
+
+    BoundaryType Triangle_2_Visual::getBoundaryType() const {
+        return m_bType;
+    }
+
+    Triangle_2 Triangle_2_Visual::KernelObject() const {
+        return Triangle_2(m_vertices[0].KernelObject(), m_vertices[1].KernelObject(), m_vertices[2].KernelObject());
+    }
+
+    Point_2_Visual Triangle_2_Visual::vertex(int i) const {
+        if(i < 0) {
+            return m_vertices[3 + i%3];
+        }
+        return m_vertices[i%3];
+    }
+} // namespace Geo2Util
--- geo2_util.h
+++ geo2_util.h
@@ -12,27 +12,24 @@ namespace Geo2Util {
     typedef K::Triangle_2 Triangle_2;
     typedef K::Iso_rectangle_2 Iso_rectangle_2;
 
-    namespace {
-        struct Color {
-            short r;
-            short g;
-            short b;
-            short trans;
-        };
-        const Color OpaqueBlack = {0, 0, 0, 255};
-        std::string toString(const Color& color);
-        
-        enum class BoundaryType : short {
-            Solid = 0,
-            Dotted = 1,
-            Dashed = 2
-        };
-    }
+    struct Color {
+        short r;
+        short g;
+        short b;
+        short trans = 255;
+    };
+    const Color OpaqueBlack = {0, 0, 0, 255};
+    
+    enum class BoundaryType : short {
+        Solid = 0,
+        Dotted = 1,
+        Dashed = 2
+    };
 
     // Core Data:
-    // point: x, y
-    // segment: source point, target point
-    // circle: center point, squared_radius
+    // point: Point_2(x, y)
+    // segment: source() point, target() point
+    // circle: center() point, squared_radius
     // triangle: p:point[0], q:point[1], r:point[2]
     // rectangle: low left point:min(), upper right point:max()
     // polygon: a list of points:vector<Point_2> (assuming)
@@ -40,12 +37,13 @@ namespace Geo2Util {
     // Wrapper classes for visulization: "KernelObject_Visual"
     // Only Allow to manipulate visual properties (color and boundary type)
     // Core properties (coordinates of vertices, squared_raduis, etc.) are Read_Only
-        // ctor: Only accept object of its Kernel type
+        // ctor: Only accept core data or its related wrapper classes
         // m_boundaryColor operations
         // m_interiorColor operations
         // m_bType operations
         // Retriece core data 
         // Optional: KernelObject() -- return a plain CGAL kernel object of the same type
+        // toString() -- Object serialization
         
     class Point_2_Visual {
     private:
@@ -56,8 +54,8 @@ namespace Geo2Util {
         Point_2 m_p;
     public:
         // Constructors
-        Point_2_Visual (const Point_2& p);
-        Point_2_Visual (const Point_2& p, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
+        Point_2_Visual(const Point_2& p);
+        Point_2_Visual(const Point_2& p, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
 
         // Visual manipulation
         void setBondaryColor(const Color& color);
@@ -73,8 +71,6 @@ namespace Geo2Util {
         // Return core data
         K::FT x() const;
         K::FT y() const;
-
-        std::string toString(const Point_2_Visual& p_visual) const;
     };
 
     class Segment_2_Visual {
@@ -86,8 +82,8 @@ namespace Geo2Util {
         Point_2_Visual m_target;
     public:
         // Constructors
-        Segment_2_Visual (const Segment_2& seg);
-        Segment_2_Visual (const Segment_2& seg, const Color& boudaryColor, const BoundaryType& btype);
+        Segment_2_Visual(const Point_2_Visual& s, const Point_2_Visual& t);
+        Segment_2_Visual(const Point_2_Visual& s, const Point_2_Visual& t, const Color& boudaryColor, const BoundaryType& btype);
 
         // Visual manipulation
         void setBondaryColor(const Color& color);
@@ -101,8 +97,6 @@ namespace Geo2Util {
         // Return core data 
         Point_2_Visual source() const;
         Point_2_Visual target() const;
-
-        std::string toString(const Segment_2_Visual& seg_visual) const;
     };
 
     class Circle_2_Visual {
@@ -115,8 +109,8 @@ namespace Geo2Util {
         K::FT m_squaredRadius;
     public:
         // Constructors
-        Circle_2_Visual (const Circle_2& p);
-        Circle_2_Visual (const Circle_2& p, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
+        Circle_2_Visual(const Point_2_Visual& center, K::FT squared_radius);
+        Circle_2_Visual(const Point_2_Visual& center, K::FT squared_radius, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
 
         // Visual manipulation
         void setBondaryColor(const Color& color);
@@ -132,8 +126,6 @@ namespace Geo2Util {
         // Return core data
         Point_2_Visual center() const;
         K::FT squared_radius() const;
-
-        std::string toString(const Circle_2_Visual& circ_visual) const;
     };
 
     class Triangle_2_Visual {
@@ -145,8 +137,8 @@ namespace Geo2Util {
         std::vector<Point_2_Visual> m_vertices; // will be initialized with size of 3
     public:
         // Constructors
-        Triangle_2_Visual (const Triangle_2& p);
-        Triangle_2_Visual (const Triangle_2& p, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
+        Triangle_2_Visual(const Point_2_Visual& p, const Point_2_Visual& q, const Point_2_Visual& r);
+        Triangle_2_Visual(const Point_2_Visual& p, const Point_2_Visual& q, const Point_2_Visual& r, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
 
         // Visual manipulation
         void setBondaryColor(const Color& color);
@@ -161,8 +153,6 @@ namespace Geo2Util {
 
         // Return core data
         Point_2_Visual vertex(int i) const;
-
-        std::string toString(const Triangle_2_Visual& tri_visual) const;
     };
 
     class Iso_rectangle_2_Visual {
@@ -175,8 +165,8 @@ namespace Geo2Util {
         Point_2_Visual m_upperRight;
     public:
         // Constructors
-        Iso_rectangle_2_Visual (const Iso_rectangle_2& p);
-        Iso_rectangle_2_Visual (const Iso_rectangle_2& p, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
+        Iso_rectangle_2_Visual(const Iso_rectangle_2& p);
+        Iso_rectangle_2_Visual(const Iso_rectangle_2& p, const Color& boudaryColor, const Color& interiorColor, const BoundaryType& btype);
 
         // Visual manipulation
         void setBondaryColor(const Color& color);
@@ -196,15 +186,22 @@ namespace Geo2Util {
         std::string toString(const Iso_rectangle_2_Visual& rect_visual) const;
     };
     // EOF Wrapper classes for visualization
+    std::string toString(const Color& color);
+    std::string toString(const BoundaryType& bt);
 
-    // original version of toString
-    std::string toString(const BoundaryType& t);
-
+    // Default toString
     std::string toString(const Point_2& p);
     std::string toString(const Segment_2& seg);
     std::string toString(const Circle_2& circ);
     std::string toString(const Triangle_2& tri);
     std::string toString(const Iso_rectangle_2& rect);
-    // EOF original version of toString
+    // EOF Default toString
+
+    // Customized toString
+    std::string toString(const Point_2_Visual& pv);
+    std::string toString(const Segment_2_Visual& segv);
+    std::string toString(const Circle_2_Visual& circv);
+    std::string toString(const Triangle_2_Visual& tri_visual);
+    // EOF Customized toString
     void printToFile(const std::string& filename, const std::vector<std::string>& geo2_Objects);
 }
